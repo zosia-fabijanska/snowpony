cb8cf7eec569061b7df9cee5eb165e8ac8947bc2
Zosia Fabijanska <zosia@good.work>
2018-06-30 21:23:54 +1000

    Add animation effect and rest of content and styling

---
+++ public/js/index.js
@@ -0,0 +1,38 @@
+$(document).ready(() => {
+
+    //Cache the body element
+    const $body = $("html, body");
+
+    //Function to scroll to page sections
+    const slowScrollTo = section => {
+        $body.animate(
+            {
+                scrollTop: $(section).offset().top
+            },
+            800
+        );
+    };
+
+    // Set Event Handlers
+    
+    $("#to-gallery").click(() => {
+        event.preventDefault();
+        slowScrollTo("#gallery");
+    });
+    
+    $("#to-aboutus").click(() => {
+        event.preventDefault();
+        slowScrollTo("#about-us");
+    });
+
+    //Scroll Reveal
+
+    window.sr = ScrollReveal();
+
+    sr.reveal('.scroll-reveal', {
+        distance: '100px',
+        duration: 800,
+        delay: 500,
+        scale: 1,
+    });
+});
